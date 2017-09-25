e370cd3cac4113816ac5eac765437caee66e3e0a
asyed141 <asyed141@outlook.com>
2017-09-25 09:24:04 -0500

    new commimt change

--- src/test/MainClass.java
+++ src/test/MainClass.java
@@ -4,7 +4,8 @@ public class MainClass {
 
 	public static void main(String[] args) {
           System.out.println("Hello world");
-          System.out.println(" Test 1 push");
+          System.out.println(" Test 1");
+          System.out.println(" new change");
 	}
 
 }
